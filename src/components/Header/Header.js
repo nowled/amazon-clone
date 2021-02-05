a1b0b970f0b8d11093d4973ee0bf955b74693de6
nowled <nowled@excite.com>
2021-02-05 23:58:53 +0300

    before name change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -35,7 +35,9 @@ const Header = () => {
       <div className='header__nav'>
         <Link to={!user && '/login'}>
           <div onClick={handleAuthentication} className='header__option'>
-            <span className='header__optionLineOne'>Hello Charles</span>
+            <span className='header__optionLineOne'>
+              <h3>{user ? user?.email : 'Member?'}</h3>
+            </span>
             <span className='header__optionLineTwo'>
               {user ? 'Sign Out' : 'Sign In'}
             </span>
